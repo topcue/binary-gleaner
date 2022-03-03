b71a9cfe54589f8478612f826d17d740465cdf48
topcue <topcue1019@naver.com>
2022-03-03 19:42:00 +0900

    Use git library

--- parser.py
+++ parser.py
@@ -1,151 +1,153 @@
+from datetime import date
 from util import *
+from commit_util import *
 
 max_page_num = 2     # set by user
 
-def get_commits_with_datetime_range(start_datetime, end_datetime):
-  # get commit_file_names with datetime range
-  file_names = os.listdir("./commits")
-  commit_file_names = []
-  for file_name in file_names:
-    commit_file_date_time = file_name[7 : 23] # get datetime part
-    if start_datetime < commit_file_date_time < end_datetime:
-      commit_file_names.append(file_name)
-  commit_file_names.sort()
-
-  # get commits(json) with datetime range
-  commits = []
-  for commit_file_name in commit_file_names:
-    commits.append(load_json("./commits/" + commit_file_name))
-  
-  return commits
-
-def get_parent_url(parent):
-  return parent["url"]
-
-def get_parent_sha(parent):
-  return parent["sha"]
-
-def get_commit_message(commit_data):
-  msg = commit_data["message"].split('\n')
-  head = msg[0]
-  body_list = msg[2:] if len(msg) >= 2 else ""
-  body = ""
-  for line in body_list:
-    body += line + "\n"
-  return [head, body]
-
-
-known_binaries = [
-  "addr2line", "ar", "bfdtest1", "bfdtest2", "cxxfilt", "elfedit", "nm", "objcopy", "objdump", "ranlib", "readelf", "size", "strings", "strip", "sysinfo"
+binutils_binaries = [
+  "addr2line", "ar", "cxxfilt", "elfedit", "nm", "objcopy", "objdump", "ranlib", "readelf", "size", "strings", "strip", "sysinfo"
 ]
 
-def parse_files_change(files):
-  # print("status:", file["status"])
-  # print("changes:", file["changes"])
+def get_commit_info(commit: Commit, file_info):
+  parent = get_commit_parent(commit)
+  # file_info = get_files_info(commit)
+  commit_info = {
+    "hash": get_commit_hash(commit),
+    "parent_hash": get_commit_hash(parent),
+    "timestamp": get_authored_datetime(commit),
+    "msg": get_commit_message(commit),
+    "changed_file_len": len(file_info),
+    "files": file_info
+  }
+  
+  return commit_info
+
+def get_changed_files_name(commit_info):
   ret = []
-  for file in files:
-    file_name = file["filename"]
-    for known_binary in known_binaries:
-      if "binutils/" + known_binary in file_name:
-        # print("filename:", file_name)
-        ret.append(known_binary)
+  for file in commit_info["files"]:
+    for known_binary in binutils_binaries:
+      if "binutils/" + known_binary in file["filename"]:
+        ret.append(file["filename"])
   return ret
 
-def parse_commit(json_data):
-  #### get child's info
-  child_sha = json_data["sha"]
-  child_url = json_data["url"]
+def foo(commit_info):
+  changed_file_names = get_changed_files_name(commit_info)
+  child_hash = commit_info["hash"]
+  parent_hash = commit_info["parent_hash"]
+  timestamp = commit_info["timestamp"]
+
+  print("\n[*] file_name:", changed_file_names)
+  print("[*] child_hash: ", child_hash)
+  print("[*] parent_hash:", parent_hash)
+
   
-  #### get parent's info
-  parent = json_data["parents"][0]
-  parent_url = get_parent_url(parent)
-  parent_sha = get_parent_sha(parent)
+
+
+  curr_path = os.getcwd()
+  base_path = curr_path + "/" + "base"
+  bin_path = "/ext/bins/bin-" + timestamp + "-" + child_hash
+  child_path = bin_path + "/" + "child"
+  parent_path = bin_path + "/" + "parent"
+  config_opt = "--disable-gdb --disable-gdbserver --disable-ld --disable-gold --disable-gas --disable-sim --disable-gprof"
   
-  #### get commit detail
-  commit_data = json_data["commit"]
-  datetime = commit_data["committer"]["date"].replace('-', '').replace(':', '')  
-  # msg = commit_data["message"]
-  # p = re.compile(r'secur|vuln|cve', re.I)
-  # feature = p.findall(msg)
-  
-  #### get file detail
-  files = json_data["files"]
-  changed_file_names = parse_files_change(files) # select file and binary here
-  
-  if not changed_file_names:
-    return 0
+  # todo: change state to json
+  if os.path.isfile(bin_path + "/state"):
+    with open(bin_path + "/state", 'r') as f:
+      state = f.read()
+      if "done" in state: 
+        print("[+] Exist. Pass.")
+        return 0
+      else:
+        print("[-] Not exist. Build it. (1)")
   else:
-    print()
-    print("[*] file_name:", changed_file_names)
-    print("[*] child_sha: ", child_sha)
-    print("[*] parent_sha:", parent_sha)
+    print("[-] Not exist. Build it. (2)")
 
-    base_path = os.getcwd()
-    origin_path = base_path + "/" + "origin"
-    bin_path = base_path + "/" + "bins/bin-" + datetime + "-" + child_sha
-    child_path = bin_path + "/" + "child"
-    parent_path = bin_path + "/" + "parent"
-    config_opt = "--disable-gdb --disable-gdbserver --disable-ld --disable-gold --disable-gas --disable-sim --disable-gprof"
-    
-    # todo: change state to json
-    if os.path.isfile(bin_path + "/state"):
-      with open(bin_path + "/state", 'r') as f:
-        state = f.read()
-        if "done" in state: 
-          print("[+] Exist. Pass.")
-          return 0
-        else:
-          print("[-] Not exist. Build it. (1)")
-    else:
-      print("[-] Not exist. Build it. (2)")
+  
 
-    # return 0
+  cmd = "\
+    mkdir -p {bin_path}                                            ;\n\
+    cd {base_path} && git checkout {child_hash}                    ;\n\
+    rm -rf {base_path}/build                                       ;\n\
+    mkdir -p {base_path}/build                                     ;\n\
+    cd {base_path}/build && {base_path}/configure -q {config_opt}  ;\n\
+    cd {base_path}/build && make > child_make.log                  ;\n\
+    cp -r {base_path}/build/binutils {bin_path}/                   ;\n\
+    cp -r {base_path}/build/child_make.log {bin_path}/             ;\n\
+    mv {bin_path}/binutils {child_path}                            ;\n\
+                                                                   \n\
+    cd {base_path} && git checkout {parent_hash}                   ;\n\
+    rm -rf {base_path}/build                                       ;\n\
+    mkdir -p {base_path}/build                                     ;\n\
+    cd {base_path}/build && {base_path}/configure -q {config_opt}  ;\n\
+    cd {base_path}/build && make > parent_make.log                 ;\n\
+    cp -r {base_path}/build/binutils {bin_path}/                   ;\n\
+    cp -r {base_path}/build/parent.log {bin_path}/                 ;\n\
+    mv {bin_path}/binutils {parent_path}                           ;\n\
+  "
+  
+  cmd = cmd.format( base_path=base_path, bin_path=bin_path,     \
+                    child_hash=child_hash, parent_hash=parent_hash,     \
+                    child_path=child_path, parent_path=parent_path, \
+                    config_opt=config_opt)
 
-    cmd = "\
-      mkdir -p {bin_path}                                             ;\n\
-      cd {origin_path} && git checkout {child_sha}                    ;\n\
-      rm -rf {origin_path}/build                                      ;\n\
-      mkdir -p {origin_path}/build                                    ;\n\
-      cd {origin_path}/build && {origin_path}/configure -q {config_opt} ;\n\
-      cd {origin_path}/build && make > child_make.log                 ;\n\
-      cp -r {origin_path}/build/binutils {bin_path}/                  ;\n\
-      cp -r {origin_path}/build/child_make.log {bin_path}/                     ;\n\
-      mv {bin_path}/binutils {child_path}                             ;\n\
-                                                                       \n\
-      cd {origin_path} && git checkout {parent_sha}                   ;\n\
-      rm -rf {origin_path}/build                                      ;\n\
-      mkdir -p {origin_path}/build                                    ;\n\
-      cd {origin_path}/build && {origin_path}/configure -q {config_opt} ;\n\
-      cd {origin_path}/build && make > parent_make.log                ;\n\
-      cp -r {origin_path}/build/binutils {bin_path}/                  ;\n\
-      cp -r {origin_path}/build/parent.log {bin_path}/                     ;\n\
-      mv {bin_path}/binutils {parent_path}                            ;\n\
-    "
-    
-    cmd = cmd.format( origin_path=origin_path, bin_path=bin_path,     \
-                      child_sha=child_sha, parent_sha=parent_sha,     \
-                      child_path=child_path, parent_path=parent_path, \
-                      config_opt=config_opt)
-    os.system(cmd)
+  print(cmd)
+  return 0
+  os.system(cmd)
 
-    is_exist = os.path.isdir(child_path) and os.path.isdir(parent_path)
-    if is_exist:
-      print("[+] Success!")
-      with open(bin_path + "/state", 'w') as outfile:
-        outfile.write("done")
-    else:
-      print("[-] Failed..")
-      exit(0)
+  is_exist = os.path.isdir(child_path) and os.path.isdir(parent_path)
+  if is_exist:
+    print("[+] Success!")
+    with open(bin_path + "/state", 'w') as outfile:
+      outfile.write("done")
+  else:
+    print("[-] Failed..")
+    exit(0)
+
+
+## TODO: Refactor me
+def filter_binutils_binary(commit: Commit):
+  file_info = get_files_info(commit)
+  for file in file_info:
+    for known_binary in binutils_binaries:
+      if "binutils/" + known_binary in file["filename"]:
+        # print("[*]", file["filename"])
+        return file_info
+  
+  return [ ]
 
 def main():
-  # get commits
-  start_datetime = datetime.datetime(2021, 1, 1, 0, 0, 0).strftime("%Y%m%dT%H%M%SZ")
-  end_datetime = datetime.datetime.now().strftime("%Y%m%dT%H%M%SZ")
-  commits = get_commits_with_datetime_range(start_datetime, end_datetime)
+  home_path = "/Users/topcue"
+  project_path = home_path + "/binary-gleaner"
+  base_path = "/Users/topcue/binary-gleaner/base" 
+  
+  ## creat base dir
+  binutils_url = get_git_repo_url_with_package_name("binutils")
+  base_repo = create_base(binutils_url, base_path)
+  print("[*] Base repo status:", get_status(base_repo, base_path))
+  print()
+  
+  ## create test dir
+  test_path = project_path + "/test"
+  test_repo = try_clone_repo(base_path, test_path)
+  repo = test_repo
 
-  # parse commits
+  ## get commits
+  commits = get_commits_with_cnt(repo, 15000)[::-1]  
+  start_datetime = datetime.datetime(2022, 1, 1)
+  end_datetime = datetime.datetime.now()
+  commits = get_commits_with_datetime_range(commits, start_datetime, end_datetime)
+
+  ## parse commits
   for commit in commits:
-    parse_commit(commit)
+    ## TODO: Find better way
+    ## Filter only if the source code - binary change relationship can be tracked
+    file_info = filter_binutils_binary(commit)
+    if not file_info:
+      continue
+    
+    commit_info = get_commit_info(commit, file_info)
+    foo(commit_info)
+
 
 if __name__ == "__main__":
   main()
--- util.py
+++ util.py
@@ -5,6 +5,7 @@ import datetime
 import urllib.parse
 import urllib.request
 import json
+from git import *
 
 commit_per_page = 30  # set by api
 
@@ -13,6 +14,9 @@ dict_repos = {
   # coreutils..
   # etc.
 }
+
+DATETIME_FORMAT = "%Y-%m-%dT%H-%M-%S"
+EMPTY_TREE_SHA = "4b825dc642cb6eb9a060e54bf8d69288fbee4904"
 
 # ================================================
 
@@ -30,19 +34,97 @@ def load_json(file_path: str):
 
 # ================================================
 
+def get_git_repo_url_with_package_name(package: str):
+  return "https://github.com/" + dict_repos[package]
+
 def get_git_commit_url_with_package_name(package: str):
   return "https://api.github.com/repos/" + dict_repos[package] + "/commits"
 
 def add_commit_url_to_page(url: str, page: int):
   return url + "?page=" + str(page)
 
-def read_json_from_url_with_token(url):
-  # get git_token manually
-  git_token = os.popen('cat $HOME/.git_token').read()[:-1]
-  headers = {"Authorization": "token " + git_token}
-  req = urllib.request.Request(url, headers = headers)
-  json_url = urllib.request.urlopen(req)
-  json_data = json.loads(json_url.read())
-  return json_data
+# def read_json_from_url_with_token(url):
+#   # get git_token manually
+#   git_token = os.popen('cat $HOME/.git_token').read()[:-1]
+#   headers = {"Authorization": "token " + git_token}
+#   req = urllib.request.Request(url, headers = headers)
+#   json_url = urllib.request.urlopen(req)
+#   json_data = json.loads(json_url.read())
+#   return json_data
 
-# ===========================================================
+# ================================================
+
+## check if path is git repo
+def is_git_repo(path):
+  if os.path.exists(path) and os.path.isdir(path):
+    try:
+      _ = Repo(path).git_dir
+      ret = True
+    except InvalidGitRepositoryError:
+      ret =  False
+  else:
+    ret = False
+  return ret
+
+## check if modified
+def is_pure_repo(repo: Repo):
+  return (not repo.untracked_files and not repo.is_dirty())
+  
+## check if modified
+def is_path_pure_repo(path: str):
+  if is_git_repo(path):
+    return is_pure_repo(Repo(path))
+  else:
+    False
+
+## get status w/o input check
+def get_status(repo, path):
+  changed = [ item.a_path for item in repo.index.diff(None) ]
+  if path in repo.untracked_files:
+    status = "untracked"
+  elif path in changed:
+    status = "modified"
+  else:
+    status = "pure"
+  return status
+
+## rm path and clone from git_url to path
+def force_clone(git_url, path):
+  print("[*] func force_clone()")
+  os.system("rm -rf " + path)
+  os.system("mkdir " + path)
+  repo = Repo.clone_from(git_url, path)
+  return repo
+
+## create base dir
+def create_base(git_url, base_path):
+  print("[*] Is '{}' pure repo?:".format(base_path), is_path_pure_repo(base_path))
+  if is_path_pure_repo(base_path):
+    repo = Repo(base_path)
+  else:
+    print("[*] call force_clone()")
+    repo = force_clone(git_url, base_path)
+  return repo
+
+## clone repo locally w/o input check
+def clone_repo(base_path, new_repo_path):
+  repo = Repo(base_path)
+  cloned_repo = repo.clone(os.path.join(base_path, new_repo_path))
+  assert cloned_repo.__class__ is Repo     # clone an existing repository
+  assert Repo.init(os.path.join(base_path, new_repo_path)).__class__ is Repo
+  return cloned_repo
+
+## reflect base paths checkout
+def try_clone_repo(base_path, new_repo_path):
+  print("[*] try_clone_repo()")
+  print("[*] base_path:", base_path)
+  print("[*] new_repo_path:", new_repo_path)
+  if is_path_pure_repo(new_repo_path):
+    repo = Repo(new_repo_path)
+  else:
+    repo = clone_repo(base_path, new_repo_path)
+  return repo
+
+
+
+# EOF
